db412536737b19e6d2d20888d722f54f0ab4982c
dsatyal1 <dineshsatyal21@gmail.com>
2024-06-20 14:26:18 -0400

    getter and setter

--- src/org/wellness/model/Subcategory.java
+++ src/org/wellness/model/Subcategory.java
@@ -1,5 +1,45 @@
 package org.wellness.model;
 
 public class Subcategory {
+	private String sub_catogory_id;
+
+	public String getSub_catogory_id() {
+		return sub_catogory_id;
+	}
+
+	public void setSub_catogory_id(String sub_catogory_id) {
+		this.sub_catogory_id = sub_catogory_id;
+	}
+
+	public String getCatogory_id() {
+		return Catogory_id;
+	}
+
+	public void setCatogory_id(String catogory_id) {
+		Catogory_id = catogory_id;
+	}
+
+	public String getSub_Catogory_type() {
+		return sub_Catogory_type;
+	}
+
+	public void setSub_Catogory_type(String sub_Catogory_type) {
+		this.sub_Catogory_type = sub_Catogory_type;
+	}
+
+	private String Catogory_id;
+	private String sub_Catogory_type;
+
+	public Subcategory(String sub_catogory_id, String catogory_id, String sub_Catogory_type) {
+		super();
+		this.sub_catogory_id = sub_catogory_id;
+		Catogory_id = catogory_id;
+		this.sub_Catogory_type = sub_Catogory_type;
+	}
+
+	public Subcategory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
